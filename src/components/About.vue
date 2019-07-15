146b56b9f67b88afe28cffd5ed2836a577b1605f
Whosc <1421851519@qq.com>
2019-07-15 10:58:55 +0800

    插槽的使用

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,11 +2,29 @@
   <div class="about">
     <h1>Hello {{ their_name }}, this is an about page</h1>
     <p>这是一个测试描述！</p>
+
+     <SlotDemo>
+        <p>default slot</p>
+        <template v-slot:title>
+          <p>title slot1</p>
+          <p>title slot2</p>
+        </template>
+        <template v-slot:item="props">
+          <p>item slot-scope {{ props.value }}</p>
+        </template>
+      </SlotDemo>
+
   </div>
 </template>
 
 <script>
+
+import SlotDemo from './SlotDemo'
+
 export default {
+  components: {
+      SlotDemo
+  },
   name: 'About',
   data() {
     return {
