a42c285b24b9255ce10fb5f353bc2df8667c456d
Whosc <1421851519@qq.com>
2019-07-15 14:31:10 +0800

    单文件模板、render函数、jsx

--- src/components/About.vue
+++ src/components/About.vue
@@ -14,16 +14,41 @@
         </template>
       </SlotDemo>
 
+      <div>
+        <p>模板、render函数、jsx</p>
+        <anchored-heading1 :level="1">Hello world!</anchored-heading1>
+        <anchored-heading2 :level="2">Hello world!</anchored-heading2>
+        <anchored-heading3 :level="3">
+          <div> 123213123  </div>
+          <div> asaaaa  </div>
+          <template v-slot:aaa>
+          <p>title slot1</p>
+          <p>title slot2</p>
+        </template></anchored-heading3>
+        
+      </div>
+
+
   </div>
 </template>
 
 <script>
 
 import SlotDemo from './SlotDemo'
+import anchoredHeading1 from './anchoredHeading1.vue'
+import anchoredHeading2 from './anchoredHeading2.js'
+import anchoredHeading3 from './anchoredHeading3.jsx'
+
+
+
+
 
 export default {
   components: {
-      SlotDemo
+      SlotDemo,
+      anchoredHeading1,
+      anchoredHeading2,
+      anchoredHeading3
   },
   name: 'About',
   data() {
